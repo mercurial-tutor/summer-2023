9ce5d2ad089b9a084582d5f030ce366b66383e0b
brandon mazey <brandon@brandons-air.home>
2023-06-27 19:17:19 -0400

    added basic image examples with and without templates

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+import os
+import random
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
-@app.route("/")
+IMG_DIR = './static'
+
+@app.route('/')
 def hello_world():
-    return "Hello, World!"
+    return 'hello world!'
+
+@app.route('/image')
+def serve_image():
+    return render_template('image.html')
+
+@app.route('/random')
+def serve_random_image():
+    img_list = os.listdir(IMG_DIR)
+    img_path = os.path.join(IMG_DIR, random.choice(img_list))
+    return send_file(img_path, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8081)
+    app.run(host='localhost', port=8081)
