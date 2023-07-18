16b6abb7dd3ee786664a2b3aebd690f050ea1ad4
brandon <b.mazey@nyu.edu>
2023-07-18 17:55:57 -0400

    added basic adventure game!

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 import os
 import random
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 IMG_DIR = './static'
 
-@app.route('/')
-def hello_world():
-    return 'hello world!'
+@app.route('/adventure')
+def adventure():
+    return render_template('adventure.html')
 
-@app.route('/image')
-def serve_image():
-    return render_template('image.html')
+@app.route('/riddle', methods = ['GET', 'POST'])
+def riddle():
+    if request.method == 'GET':
+        return render_template('riddle.html')
+    if request.method == 'POST':
+        form_data = request.form
+        if form_data['answer'] != 'nothing':
+            return redirect('/lose', code = 307)
+        return redirect('/win', code = 307)
 
-@app.route('/random')
-def serve_random_image():
-    img_list = os.listdir(IMG_DIR)
-    img_path = os.path.join(IMG_DIR, random.choice(img_list))
-    return send_file(img_path, mimetype='image/png')
+@app.route('/win', methods = ['POST'])
+def win():
+    if request.method == 'POST':
+        return 'You win!'
+
+@app.route('/lose', methods = ['POST'])
+def lose():
+    if request.method == 'POST':
+        return 'You lose!'
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8081)
